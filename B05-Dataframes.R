066ae490d85b82f608a810dfd7dc688418c6145c
Dublin R <DublinRUserGroup@gmail.com>
2013-10-12 00:18:11 +0100

    Update and rename Dataframes to B05-Dataframes.R

--- B05-Dataframes.R
+++ B05-Dataframes.R
@@ -1,5 +1,6 @@
 
-# creating a data frame
+################################################
+# Creating a more complex data frame
 
 A <- data.frame(
       name=c("a","b","c"),
@@ -12,4 +13,13 @@ A <- data.frame(
       x=c(2.2,3.3,4.4),
       date=as.Date(c("2004-04-04","2005-05-05","2006-06-06"))
 )
+#################################################
+
+# Characteristics of a Data Frame
+
+summary(A)
+dim(A)
+nrow(A) ; ncol(A)
+names(A)
+
 #################################################
