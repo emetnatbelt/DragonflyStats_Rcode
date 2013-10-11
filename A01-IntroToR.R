8669a7c68ffe7e0006ab8076119af3e97d858147
Dublin R <DublinRUserGroup@gmail.com>
2013-10-12 00:16:06 +0100

    Create A01-IntroToR.R

---
+++ A01-IntroToR.R
@@ -0,0 +1,11 @@
+# A01 Introduction to R
+##########################
+# part i  Assignment 
+# part ii Basic mathematical operations
+# part iii Creating a simple numeric vector
+# Part iv Characteristics of a vector
+# Part v Basic statistical operations
+#########################
+ 
+x<-14
+y<-8
