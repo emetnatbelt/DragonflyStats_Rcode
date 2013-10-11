bfc6813e47631bc738d2b1f229f4a2cbf32c68a6
Dublin R <DublinRUserGroup@gmail.com>
2013-10-12 00:23:26 +0100

    Update and rename REnvironment to A02-TheREnvironment.R

---
+++ A02-TheREnvironment.R
@@ -0,0 +1,19 @@
+# Computing in the R Environment
+
+demo(graphics)
+
+getwd()
+
+list.files(getwd())
+
+ls()
+
+history()
+
+sessionInfo()
+
+#list of currently available data sets
+data()
+
+
+############################################################
