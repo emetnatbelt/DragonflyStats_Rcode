3aee0bbb29cc93dccb207182ce9708b6608b9a20
Dublin R <DublinRUserGroup@gmail.com>
2013-10-12 00:21:02 +0100

    Update and rename BasicStats to A10-BasicStats.R

--- A10-BasicStats.R
+++ A10-BasicStats.R
@@ -1,14 +1,17 @@
- #  Basic Statistical Procedures with R
+ # Section A 10 Basic Statistical Procedures with R
+ # Basic Statistical Procedures with R
 
 ##################################################################
-
+# simulate 100 rolls of a die
 dice=c(rep(1,17),rep(2,16),rep(3,15),rep(4,19),rep(5,18),rep(6,15))
 
+table(dice)
 mean(dice)
 sd(dice)
 median(dice)
 
 ##################################################################
+# Constructing a Histogram
 
 X=c(37,	44,	48,	51,	53,	56,	58,	60,	62,	63,
 65,	67,	69,	70,	72,	74,	76,	77,	79,	81,
