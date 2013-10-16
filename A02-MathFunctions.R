bb6a80d1dfba40032012ce4771e38b43f4dee6af
Dublin R <DublinRUserGroup@gmail.com>
2013-10-16 20:28:36 +0100

    Update and rename A02 Mathematical functions for R to A02-MathFunctions.R

---
+++ A02-MathFunctions.R
@@ -0,0 +1,36 @@
+# Mathematical functions for R
+# - Simple Mathematical functions
+# - Trigonometric functions
+# - Matrices operations
+# - Complex numbers
+
+#####################################################
+#Creating a vector
+# c() - generic function to combine arguments 
+# with the default forming a vector
+
+#####################################################
+# Data Selection and manipulation
+
+sort(x)
+rev(x)
+#####################################################
+# Trigonometrics functions
+
+pi            #   pi to 6 decimal places.
+# - sin(), abs(), cos(), exp, log(), log()
+# - complex numbers
+
+	* abs, sign, sqrt, floor, ceiling, trunc, round, signif
+	* exp, log, expm1, log1p, cos, sin, tan, acos, asin, atan, cosh, sinh, tanh, acosh, asinh, atanh
+	* lgamma, gamma, digamma, trigamma
+	* cumsum, cumprod, cummax, cummin 
+
+
+tan() ,cos(), sin()   - Common trigonometric functions
+
+
+
+#####################################################
+
+
