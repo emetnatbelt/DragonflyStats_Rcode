ae53d159649b4e5078b8bcff84d93265b97da1ee
Dublin R <DublinRUserGroup@gmail.com>
2013-10-16 20:04:04 +0100

    Create A11-Precision.R

---
+++ A11-Precision.R
@@ -0,0 +1,19 @@
+# Managing Precision
+# Also - Absolute Value Function
+
+# floor() 	   The floor function 		
+# ceiling()    The ceiling function       
+# round()      Round to the nearest integer
+# round( ,2)   Round to two decimal places
+# abs()        Absolute Value Function
+
+##################################################
+
+floor(pi)
+ceiling(pi)
+
+##################################################
+
+# Exercise:  write pi  with 4 decimal places.
+round(pi,4)
+
