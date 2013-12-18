f6ee4c967546a4bdf4216f75bb1511169c4d8692
Dublin R <DublinRUserGroup@gmail.com>
2013-12-18 15:59:21 +0000

    Create cars.r

---
+++ Part-B/cars.r
@@ -0,0 +1,2 @@
+hist(speed,col=c("blue","red"),main=paste("Histogram of the speed of the cars"))
+boxplot(cars,col=c("yellow","green"),main=paste("boxplotsof speed and stopping distance of the cars"))
