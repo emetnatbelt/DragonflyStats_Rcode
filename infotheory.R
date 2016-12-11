511e52335d8c8e5af0033c90d5828afd9e93653d
DragonflyStats <kobriendublin@gmail.com>
2016-12-11 10:47:57 +0000

    Create infotheory.R

---
+++ infotheory.R
@@ -0,0 +1,66 @@
+
+
+For Loops
+If and Else
+Relational  and Logical operators
+Letters
+Information Theory
+
+
+
+
+For Loops
+
+If and Else
+
+Relational  and Logical operators 
+
+
+Relational operators
+
+
+==	equality
+<	less than
+>         greater than
+>=       greater than or equal to
+<=       less than or equal to
+
+
+Logical operators
+
+
+
+Letters
+
+
+names = Letters[1:n]
+
+
+
+Information Theory :  Entropy
+
+
+X = c( 0.35, 0.25, 0.20, 0.12, 0.05, 0.03)
+
+
+sum(X)==1	# True or False
+
+
+# base  = exp(1) , 2 , 10
+
+
+b=exp(1)
+
+
+-X * log(X,b)
+
+
+sum(-X * log(X,b))
+
+
+
+
+
+H(X) is the entopy of variable X
+
+
